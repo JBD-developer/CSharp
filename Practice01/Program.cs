b0589c4147b7e61dd50281c30533266a6dee82e3
JBD <lio222@hanmail.net>
2021-03-09 08:51:40 +0900

    2021-03-09 VS C# Console Write

--- Practice01/Program.cs
+++ Practice01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Practice01
 {
@@ -12,6 +13,13 @@ namespace Practice01
         {
             Console.WriteLine("Git Test");
             Console.WriteLine("Commit Test");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법 : Practice01");
+                return;
+            }
+            WriteLine("Hello , {0}", args[0]);
         }
     }
 }
