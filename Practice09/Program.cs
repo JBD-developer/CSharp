6aad7e35a9cf67759716b0e2563edc86c5cf0165
JBD <lio222@hanmail.net>
2021-03-22 13:38:56 +0900

    2021-03-22 C# DateTime

---
+++ Practice09/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using static System.Console;
+namespace Practice09
+{
+    //C# DateTime
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 03, 22, 13, 14, 00);
+            //12시간 형식 2021-03-22 오후 13:14:00(월)
+            Console.WriteLine("12시간 형식 : {0:yyyy-MM-dd tt hh:mm:ss(ddd)}", dt);
+
+            //24시간 형식 2021-03-22 오후 23:14:00(월)
+            Console.WriteLine("24시간 형식 : {0:yyyy-MM-dd HH:mm:ss(dddd)}", dt);
+            Console.WriteLine();
+
+            //CultureInfo class 
+            CultureInfo cultureInfoKor = new CultureInfo("ko-KR");
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", cultureInfoKor));
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (dddd)", cultureInfoKor));
+            WriteLine(dt.ToString(cultureInfoKor));
+            Console.WriteLine();
+
+            CultureInfo cultureInfoEng = new CultureInfo("en-US");
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", cultureInfoEng));
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (dddd)", cultureInfoEng));
+            WriteLine(dt.ToString(cultureInfoEng));
+
+        }
+    }
+}
--- Practice09/Properties/AssemblyInfo.cs
+++ Practice09/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Practice08_2")]
+[assembly: AssemblyTitle("Practice09")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Practice08_2")]
+[assembly: AssemblyProduct("Practice09")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("61a9c467-db5f-40ab-9ab0-a8439a8a41c0")]
+[assembly: Guid("ec89eb03-1c64-421b-976b-2dcf86c3f633")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
