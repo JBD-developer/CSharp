c41283374dbfae840f3849090a56c37b495a6cb9
JBD <lio222@hanmail.net>
2021-03-22 18:30:27 +0900

    2021-03-22 C# Method

---
+++ Practice10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice10
+{
+    class Program
+    {
+        private static int LF_Add(int p_iNum1, int p_iNum2)
+        {
+            return p_iNum1 + p_iNum2;
+
+        }
+        private static int LF_Sub(int p_iNum1, int p_iNum2)
+        {
+            return p_iNum1 - p_iNum2;
+
+        }
+        private static int LF_Mul(int p_iNum1, int p_iNum2)
+        {
+            return p_iNum1 * p_iNum2;
+
+        }
+        private static double LF_Div(double p_iNum1, double p_iNum2)
+        {
+            return p_iNum1 / p_iNum2;
+
+        }
+        static void Main(string[] args)
+        {
+            int iTempAddResult = LF_Add(300, 2);
+            int iTempSubResult = LF_Sub(200, 12);
+            int iTempMulResult = LF_Mul(400, 40);
+            double iTempDivResult = LF_Div(4123, 130);
+
+            Console.WriteLine($"iTempAddResult = {iTempAddResult}");
+            Console.WriteLine($"iTempSubResult = {iTempSubResult}");
+            Console.WriteLine($"iTempMulResult = {iTempMulResult}");
+            Console.WriteLine($"iTempDivResult = {iTempDivResult}");
+
+        }
+    }
+}
---
+++ Practice10_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice10_1
+{
+    class Program
+    {
+        private static int LF_Fibonacci(int p_iNum)
+        {
+            if(p_iNum < 2)
+            {
+                return p_iNum;
+            }
+            else
+            {
+                return LF_Fibonacci(p_iNum - 1) + LF_Fibonacci(p_iNum - 2);
+
+            }
+        }
+        private static void LF_ProfilePrint(string p_sName)
+        {
+            if(p_sName == "")
+            {
+                return;
+            }
+            Console.WriteLine($"{p_sName}");
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("이름을 입력해주세요");
+            LF_ProfilePrint(Console.ReadLine());
+            Console.WriteLine("숫자를 입력해주세요");
+            int iTempNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"InputNumber:{iTempNum} Fibonacci Number:{LF_Fibonacci(iTempNum)}");
+        }
+    }
+}
