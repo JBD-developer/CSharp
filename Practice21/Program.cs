f93538bd493b04c6610a290174be38ab70914513
JBD <lio222@hanmail.net>
2021-04-08 20:09:01 +0900

    2021-04-08 C# Property

---
+++ Practice21/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Property;
+namespace Property
+{
+    //Property 
+    class BirtyDayInfo
+    {
+        private string _sName;
+        private DateTime _dBirthDay;
+    
+        public string Name
+        {
+            get
+            {
+                return _sName;
+            }
+            set
+            {
+                _sName = value;
+            }
+        }
+
+        public DateTime BirtyDay
+        {
+            get
+            {
+                return _dBirthDay;
+            }
+            set
+            {
+                _dBirthDay = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(_dBirthDay).Ticks).Year;
+            }
+        }
+    }
+}
+namespace Practice21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BirtyDayInfo clsBirthDayInfo = new BirtyDayInfo();
+            clsBirthDayInfo.Name = "Kim";
+            clsBirthDayInfo.BirtyDay = new DateTime(1991, 6, 21);
+
+            Console.WriteLine($"Name : {clsBirthDayInfo.Name}");
+            Console.WriteLine($"BirtyDay : {clsBirthDayInfo.BirtyDay} ");
+            Console.WriteLine($"Age : {clsBirthDayInfo.Age}");
+        }
+    }
+}
---
+++ Practice21_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoImplementedProperty;
+
+namespace AutoImplementedProperty
+{
+    class BirthDayInfo
+    {
+        public string Name { get; set; } = "UnKnown";
+        public DateTime BirthDay { get; set; } = new DateTime(1, 1, 1);
+
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(BirthDay).Ticks).Year;
+            }
+        }
+    }
+}
+namespace Practice21_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BirthDayInfo clsBirthDayInfo = new BirthDayInfo();
+            Console.WriteLine($"Name : {clsBirthDayInfo.Name}");
+            Console.WriteLine($"BirthDay : {clsBirthDayInfo.BirthDay}");
+            Console.WriteLine($"Age:{clsBirthDayInfo.Age}");
+
+            clsBirthDayInfo.Name = "JEONG";
+            clsBirthDayInfo.BirthDay = new DateTime(1987, 11, 28);
+
+            Console.WriteLine($"Name : {clsBirthDayInfo.Name}");
+            Console.WriteLine($"BirthDay : {clsBirthDayInfo.BirthDay.ToShortDateString()}");
+            
+        }
+    }
+}
