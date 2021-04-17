c1185236477a49e6672d869096296d1be2295588
JBD <lio222@hanmail.net>
2021-04-17 15:43:39 +0900

    2021-04-17 C# Array

---
+++ Practice22_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Practice22_3
+{
+    class Program
+    {
+        //Array Slice
+        
+        static void PrintArray(Array array)
+        {
+            foreach(var e in array)
+            {
+                Console.WriteLine($"e :{e}");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            char[] cArray = new char['Z'-'A' + 1 ];
+
+            for(int i=0; i<cArray.Length; i++)
+            {
+                cArray[i] = (char)('A' + i);
+            }
+
+            PrintArray(cArray);
+           
+        }
+    }
+}
