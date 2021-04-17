6127dd93926e1955dda37f94825a6941167282c0
JBD <lio222@hanmail.net>
2021-04-17 14:39:54 +0900

    2021-04-17 C# Array

---
+++ Practice22/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice22
+{
+    //Array
+    //배열은 데이터를 담는 상자와 같아서 필요한 용량을 가진 배열을 만들어서 담는다 
+    //배열은 각 요소에 데이터를 저장, 요소 안에 있는 데이터를 읽어올 때 배열 이름 뒤에 괄호 [] 를 붙이고 
+    //[] 사이에는 indexx를 적어주면 된다 주의할 점은 index는 1부터가 아니라 0부터 시작한다는 것이다 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rdm = new Random();
+            int[] score = new int[5];
+            for(int i=0; i<score.Length; i++)
+            {
+                score[i] = rdm.Next(0, 10);
+                Console.WriteLine(score[i]);
+            }
+
+            int totalScore = 0; 
+            double avgScore = 0;
+            
+            int[] score2 = new int[10];
+            foreach(int scoreValue in score)
+            {
+                totalScore += scoreValue;
+                Console.WriteLine($"score={scoreValue}");
+            }
+            Console.WriteLine($"Total Score={totalScore}");
+
+            avgScore =(double) totalScore / score.Length;
+
+            Console.WriteLine($"Average Score={avgScore}");
+        }
+    }
+}
---
+++ Practice22_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice22_1
+{
+    class Program
+    {
+        //Array
+        //배열을 초기화 하는 방법 세가지 
+        string[] _sArray = new string[3] { "C#", ".Net", "4.6" };
+        char[] _cArray = new char[] { 'A', 'B', 'C' };
+        int[] _iArray = { 12, 13, 14 };
+
+        //System.Array 
+        static void Main(string[] args)
+        {
+            int[] _iArray = new int[] { 10, 20, 30, 40, 50 };
+            Console.WriteLine($"Type Of Array : {_iArray.GetType()}");
+            Console.WriteLine($"Base Type of Array : {_iArray.GetType().BaseType}");
+        }
+    }
+}
