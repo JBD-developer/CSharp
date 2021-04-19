e333b426efc5b3cff2107cbaff6fae9789c89200
JBD <lio222@hanmail.net>
2021-04-19 20:27:14 +0900

    2021-04-19 C# Generic

---
+++ Practice25/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice25
+{
+    //Generic Progeramming 
+    //특수한 개념으로부터 공통된 개념을 찾아 묶는 것을 Generalization이라고 한다 
+    //Generic Method는 이름처럼 Data Type을 일반화한 Method이다 
+    //
+    class Program
+    {
+        static void CopyArray<T>(T[]source, T[] target)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                target[i] = source[i];
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int[] source = { 1, 2, 3, 4, 5, 6 };
+            int[] target = new int[source.Length];
+
+            CopyArray<int>(source, target);
+
+            foreach(int element in target)
+            {
+                Console.WriteLine($"element : {element}");
+            }
+
+            string[] source2 = { "One", "Two", "Three", "Four", "Five" };
+            string[] target2 = new string[source2.Length];
+
+            CopyArray<string>(source2, target2);
+
+            foreach(string element in target2)
+            {
+                Console.WriteLine($"element : {element}");
+            }
+        }
+    }
+}
---
+++ Practice25_1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using GenericTest;
+
+namespace GenericTest
+{
+    class MyList<T>
+    {
+        private T[] _tArray; 
+
+        public MyList()
+        {
+            _tArray = new T[3];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return _tArray[index];
+            }
+            set
+            {
+                if(index >= _tArray.Length)
+                {
+                    Array.Resize<T>(ref _tArray, index + 1);
+                    Console.WriteLine($"_tArray Resized : {_tArray.Length}");
+                }
+                _tArray[index] = value;
+            }
+        }
+        public int Length
+        {
+            get
+            {
+                return _tArray.Length;
+            }
+        }
+    }
+}
+namespace Practice25_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> str_list = new MyList<string>();
+            str_list[0] = "abc";
+            str_list[1] = "def";
+            str_list[2] = "ghi";
+
+            for (int i = 0; i < str_list.Length; i++)
+            {
+                Console.WriteLine($"str_list[{i}] : {str_list[i]}");
+            }
+
+            MyList<int> int_list = new MyList<int>();
+            int_list[0] = 1;
+            int_list[1] = 2;
+            int_list[2] = 3;
+
+            for (int i = 0; i < int_list.Length; i++)
+            {
+                Console.WriteLine($"int_list[{i}]: {int_list[i]}");
+            }
+        }
+    }
+}
