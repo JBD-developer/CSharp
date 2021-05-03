62fd422704e8565807594363a4808acf33f7297a
JBD <lio222@hanmail.net>
2021-05-04 07:29:52 +0900

    2021-05-04 C# Generic

---
+++ Practice25_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice25_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            dic["하나"] = "One";
+            dic["둘"] = "Two";
+            dic["셋"] = "Three";
+            dic["넷"] = "Four";
+            dic["다섯"] = "Five";
+
+
+            Console.WriteLine(dic["하나"]);
+            Console.WriteLine(dic["둘"]);
+            Console.WriteLine(dic["셋"]);
+            Console.WriteLine(dic["넷"]);
+            Console.WriteLine(dic["다섯"]);
+
+        }
+    }
+}
