4056618baf241e8ca151dd9af4be14cc34b987c0
JBD <lio222@hanmail.net>
2021-05-10 07:51:53 +0900

    2021-05-10 C# Delegate

---
+++ Practice27_2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice27_2
+{
+    delegate int Compare<T>(T a, T b);
+    class Program
+    {
+        static int AscendCompate<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b);
+        }
+        static int DeScendCompare<T>(T a , T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) * -1;
+        }
+        static void BubbleSort<T>(T[] DataSet, Compare<T> Comparer)
+        {
+            int i = 0;
+            int j = 0;
+            T temp;
+            for (i = 0; i < DataSet.Length-1; i++)
+            {
+                for (j = 0 ; j < DataSet.Length-(i+1); j++)
+                {
+                    if (Comparer(DataSet[j], DataSet[j+1]) >0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp; 
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 34, 5, 56, 7 };
+            Console.WriteLine("Sorting Ascending");
+            BubbleSort<int>(array, new Compare<int>(AscendCompate));
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]}");
+            }
+
+            string[] array2 = { "C#", "JAVA", "PYTHON", "JAVASCRIPT", "HTML"};
+            
+            Console.WriteLine("Sorting Descending");
+
+            BubbleSort<string>(array2, new Compare<string>(DeScendCompare));
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine($"{array2[i]}");
+            }
+        }
+    }
+}
---
+++ Practice27_3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice27_3
+{
+    delegate void Notify(string message);
+    class Notifier
+    {
+        public Notify EventOccured;
+    }
+
+    class EventListener
+    {
+        private string name;
+        public EventListener(string name)
+        {
+            this.name = name; 
+        }
+        public void SomethingHapped(string message)
+        {
+            Console.WriteLine($"{name}.SomethingHappend : {message}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notifier notifier = new Notifier();
+            EventListener listener1 = new EventListener("Listner1");
+            EventListener listener2 = new EventListener("Listner2");
+            EventListener listener3 = new EventListener("Listner3");
+
+            notifier.EventOccured += listener1.SomethingHapped;
+            notifier.EventOccured += listener2.SomethingHapped;
+            notifier.EventOccured += listener3.SomethingHapped;
+
+            notifier.EventOccured("You've Got Mail");
+
+            Console.WriteLine();
+
+            notifier.EventOccured -= listener1.SomethingHapped;
+            notifier.EventOccured("Download Complete");
+
+            Console.WriteLine();
+
+            notifier.EventOccured = new Notify(listener2.SomethingHapped) + new Notify(listener3.SomethingHapped);
+            notifier.EventOccured("Nuclear launch detected");
+
+            Console.WriteLine();
+
+            Notify notify1 = new Notify(listener1.SomethingHapped);
+            Notify notify2 = new Notify(listener2.SomethingHapped);
+
+            notifier.EventOccured = (Notify)Delegate.Combine(notify1, notify2);
+            notifier.EventOccured("Fire");
+
+            Console.WriteLine();
+
+            notifier.EventOccured = (Notify)Delegate.Remove(notifier.EventOccured, notify2);
+            notifier.EventOccured("RPG");
+
+        }
+    }
+}
