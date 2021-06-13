8cc55f9cad9fe7344b75baa9773c5f949ce61245
JBD <lio222@hanmail.net>
2021-06-13 17:45:17 +0900

    2021-06-13 C# Delegate

---
+++ Practice27_00/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice27_00
+{
+    class Program
+    {
+        private delegate int RunCalc(int a, int b);
+
+        private static int LF_Sum(int p_num1, int p_num2)
+        {
+            return p_num1 + p_num2;
+        }
+
+        private static int LF_Mul(int p_num1, int p_num2)
+        {
+            return p_num1 * p_num2;
+        }
+
+        static void Main(string[] args)
+        {
+            RunCalc run = LF_Sum;
+            Console.WriteLine(run(1, 4));
+        }
+    }
+}
