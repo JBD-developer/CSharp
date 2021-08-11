f6f20b189dbad9046dbd3681d23a4e21a3db7004
JBD <lio222@hanmail.net>
2021-08-12 07:56:34 +0900

    2021-08-12 C# Stream

---
+++ Practice28_1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice28_1
+{
+    class Program
+    {
+        static void LF_OnWrongPathType(string p_sType)
+        {
+            Console.WriteLine($"{p_sType} is wrong type");
+            return;
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Usage : Practice28_1.exe<Path>[Type:File/Directory]");
+                return;
+            }
+            string sPath = args[0];
+            string sType = "File";
+            if (args.Length > 1)
+            {
+                sType = args[1];
+            }
+
+            if (File.Exists(sPath)||Directory.Exists(sPath))
+            {
+                if (sType == "File")
+                {
+                    File.SetLastWriteTime(sPath, DateTime.Now);
+                }
+                else if(sType == "Directory")
+                {
+                    Directory.SetLastWriteTime(sPath, DateTime.Now);
+                }
+                else
+                {
+                    LF_OnWrongPathType(sPath);
+                    return;
+                }
+                Console.WriteLine($"Updated {sPath} {sType}");
+            }
+            else
+            {
+                if (sType == "File")
+                {
+                    File.Create(sPath).Close();
+                }
+                else if (sType == "Directory")
+                {
+                    Directory.CreateDirectory(sPath);
+                }
+                else
+                {
+                    LF_OnWrongPathType(sPath);
+                    return;
+                }
+                Console.WriteLine($"Created {sPath}, {sType}");
+            }
+        }
+    }
+}
---
+++ Practice28_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice28_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long lSomeValue = 0x123456789ABCDEF0;
+            Console.WriteLine($"{"Original Data", -1} : 0x{lSomeValue:X16}");
+
+            Stream outStream = new FileStream("a.dat", FileMode.Create);
+            byte[] arrayWritrByte = BitConverter.GetBytes(lSomeValue);
+
+            Console.WriteLine($"{"Byte Array",-13}:");
+
+            foreach (var value in arrayWritrByte)
+            {
+                Console.WriteLine($"{value:X2}\r\n");
+            }
+            Console.WriteLine();
+
+            outStream.Write(arrayWritrByte, 0, arrayWritrByte.Length);
+            outStream.Close();
+
+            Stream inStream = new FileStream("a.dat", FileMode.Open);
+            byte[] arrayReadByte = new byte[8];
+            int i = 0;
+            while (inStream.Position < inStream.Length)
+            {
+                arrayReadByte[i++] = (byte)inStream.ReadByte();
+            }
+            long lReadValue = BitConverter.ToInt64(arrayReadByte, 0);
+            Console.WriteLine($"{"Read Data",-13} : 0x{lReadValue:X16}");
+            Console.ReadLine();
+        }
+    }
+}
