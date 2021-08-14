fea5ec58d754c021d7bb9c5e00b9e77fde7b71a0
JBD <lio222@hanmail.net>
2021-08-14 17:02:37 +0900

    2021-08-14 C# Stream

---
+++ Practice28_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice28_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = File.Create("a.txt");
+            FileInfo fileInfo = new FileInfo("b.txt");
+            FileStream fileStream1 = fileInfo.Create();
+
+            fileStream.Close();
+            fileStream1.Close();
+            File.Copy("a.txt", "c.txt");
+            FileInfo fileInfo2 = fileInfo.CopyTo("d.txt");
+            
+        }
+    }
+}
---
+++ Practice28_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice28_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("a.txt");
+            sw.Write(Console.ReadLine());
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            while (key.Key != ConsoleKey.Escape)
+            {
+                sw.WriteLine(Console.ReadLine());
+            }
+            sw.Close();
+
+            StreamReader sr = new StreamReader("a.txt");
+            while (sr.Peek()>0)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
