ca8297fe3b0e86e0d1f4eab4855d3fc12085d978
JBD <lio222@hanmail.net>
2021-08-16 15:20:25 +0900

    2021-08-16 C# StreamReader, StreamWriter

---
+++ Practice28_5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice28_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sLine = string.Empty;
+            using(StreamReader reader = new StreamReader(@"E:\workSpace_Etc\data.txt"))
+            {
+                while ((sLine = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(sLine);
+                }
+            }
+            using (StreamReader reader = new StreamReader(@"E:\workSpace_Etc\data.txt"))
+            {
+                int iChar = reader.Read();
+                Console.WriteLine($"iChar : {iChar.ToString()}");
+                
+                char[] arrayBuffer = new char[10];
+
+                int iReadCount = reader.Read(arrayBuffer, 0, arrayBuffer.Length);
+                for (int i = 0; i < arrayBuffer.Length; i++)
+                {
+                    Console.WriteLine($"arrayBuffer{i} = {arrayBuffer[i]}");
+                }
+                Console.WriteLine(arrayBuffer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Practice28_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice28_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = new StreamWriter(@"E:\workSpace_Etc\data.txt"))
+            {
+                writer.WriteLine("Line 1");
+                writer.WriteLine("Line 1");
+
+                decimal val = 1024.10m;
+                writer.WriteLine(val);
+                char [] array = new char[5] { 'A', 'B', 'C', 'D', 'E' };
+                writer.WriteLine(array);
+            }
+        }
+    }
+}
