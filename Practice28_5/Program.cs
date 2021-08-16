bee57504363288af27abe3bf35f07a72d63033cc
JBD <lio222@hanmail.net>
2021-08-16 15:40:02 +0900

    2021-08-16 C# File Reader, Writer

--- Practice28_5/Program.cs
+++ Practice28_5/Program.cs
@@ -11,30 +11,49 @@ namespace Practice28_5
     {
         static void Main(string[] args)
         {
-            string sLine = string.Empty;
-            using(StreamReader reader = new StreamReader(@"E:\workSpace_Etc\data.txt"))
-            {
-                while ((sLine = reader.ReadLine()) != null)
-                {
-                    Console.WriteLine(sLine);
-                }
-            }
-            using (StreamReader reader = new StreamReader(@"E:\workSpace_Etc\data.txt"))
-            {
-                int iChar = reader.Read();
-                Console.WriteLine($"iChar : {iChar.ToString()}");
-                
-                char[] arrayBuffer = new char[10];
+            //StreamReader 
 
-                int iReadCount = reader.Read(arrayBuffer, 0, arrayBuffer.Length);
-                for (int i = 0; i < arrayBuffer.Length; i++)
-                {
-                    Console.WriteLine($"arrayBuffer{i} = {arrayBuffer[i]}");
-                }
-                Console.WriteLine(arrayBuffer);
+            //string sLine = string.Empty;
+            //using(StreamReader reader = new StreamReader(@"E:\workSpace_Etc\data.txt"))
+            //{
+            //    while ((sLine = reader.ReadLine()) != null)
+            //    {
+            //        Console.WriteLine(sLine);
+            //    }
+            //}
+            //using (StreamReader reader = new StreamReader(@"E:\workSpace_Etc\data.txt"))
+            //{
+            //    int iChar = reader.Read();
+            //    Console.WriteLine($"iChar : {iChar.ToString()}");
 
-                Console.ReadLine();
+            //    char[] arrayBuffer = new char[10];
+
+            //    int iReadCount = reader.Read(arrayBuffer, 0, arrayBuffer.Length);
+            //    for (int i = 0; i < arrayBuffer.Length; i++)
+            //    {
+            //        Console.WriteLine($"arrayBuffer{i} = {arrayBuffer[i]}");
+            //    }
+            //    Console.WriteLine(arrayBuffer);
+
+            //    Console.ReadLine();
+            //}
+
+            //FileReader 
+            string sData = File.ReadAllText(@"E:\workSpace_Etc\data.txt");
+            Console.WriteLine($"sData = {sData}");
+
+            string[] arrLine = File.ReadAllLines(@"E:\workSpace_Etc\data.txt");
+            for (int i = 0; i < arrLine.Length; i++)
+            {
+                Console.WriteLine($"arrLine{i} : {arrLine[i]}");
             }
+
+            IEnumerable<string> lines = File.ReadLines(@"E:\workSpace_Etc\data.txt");
+            foreach(string line in lines)
+            {
+                Console.WriteLine($"{line}");
+            }
+            Console.ReadLine();
         }
     }
 }
--- Practice28_6/Program.cs
+++ Practice28_6/Program.cs
@@ -11,16 +11,34 @@ namespace Practice28_6
     {
         static void Main(string[] args)
         {
-            using (StreamWriter writer = new StreamWriter(@"E:\workSpace_Etc\data.txt"))
-            {
-                writer.WriteLine("Line 1");
-                writer.WriteLine("Line 1");
+            //StreamWriter 
+            //using (StreamWriter writer = new StreamWriter(@"E:\workSpace_Etc\data.txt"))
+            //{
+            //    writer.WriteLine("Line 1");
+            //    writer.WriteLine("Line 1");
 
-                decimal val = 1024.10m;
-                writer.WriteLine(val);
-                char [] array = new char[5] { 'A', 'B', 'C', 'D', 'E' };
-                writer.WriteLine(array);
+            //    decimal val = 1024.10m;
+            //    writer.WriteLine(val);
+            //    char [] array = new char[5] { 'A', 'B', 'C', 'D', 'E' };
+            //    writer.WriteLine(array);
+            //}
+
+            //FileWriter
+            string sData = File.ReadAllText(@"E:\workSpace_Etc\data.txt");
+            Console.WriteLine($"sData = {sData}");
+            File.WriteAllText("data.out", sData);
+
+            string[] arrWriteLine = { "C#", "is", "more", "than", "Java" };
+            File.WriteAllLines("data2.txt", arrWriteLine);
+            File.AppendAllText("data2.txt", sData);
+
+            string[] arrReadLine = File.ReadAllLines(@"E:\workSpace_Etc\data.txt");
+            for (int i = 0; i < arrReadLine.Length; i++)
+            {
+                Console.WriteLine($"arrLine{i} : {arrReadLine[i]}");
             }
+            File.AppendAllLines("data2.txt", arrReadLine);
+            Console.ReadLine();
         }
     }
 }
